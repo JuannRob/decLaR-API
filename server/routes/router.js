a81764613c77ca3e60dc6e0b2fdf7a8f31ca1c9b
Juan Robledo <juannrob@gmail.com>
2022-11-16 12:08:22 -0300

    minor fix

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAll, create, getData2 } = require('../controller/decretosController')
+const { getAll, create } = require('../controller/decretosController')
 
 router.get("/", getAll);
 
 router.post("/decretos", create);
 
-router.get("/data2", getData2);
 
 module.exports = router;

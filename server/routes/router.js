f8d9ecfc8e7fcac6da9f032eeee889b7e695408a
Juan Robledo <juannrob@gmail.com>
2023-02-18 17:20:50 -0300

    minor

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { buscarDecretos, crear, verDecreto, crearVarios } = require('../controller/decretosController');
 
-
 router.get("/", (req, res) => {
     res.render('search')
 });

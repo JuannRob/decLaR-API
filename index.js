4e7a0de73c206f030143b7fc58d6a6b09bfbd2ed
Juan Robledo <juannrob@gmail.com>
2023-08-28 14:38:09 -0300

    Dependencies fix

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const favicon = require('serve-favicon');
-const serveStatic = require('serve-static');
 const cors = require("cors");
 const connectDB = require('./server/database/connection');
 const routes = require('./server/routes/router');
@@ -18,13 +16,6 @@ app.use(morgan('tiny'));
 
 //parse request to body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//set view engine
-app.set("view engine", "ejs");
-
-//set public folder
-app.use('/', serveStatic(__dirname + '/public'));
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 //mongodb connection
 connectDB();

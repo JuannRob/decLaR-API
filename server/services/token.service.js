e26581fba899c0e6c51d6ceeecd718bcef7bbe4b
Juan Robledo <juannrob@gmail.com>
2024-04-04 19:20:49 -0300

    rename api name variable

--- server/services/token.service.js
+++ server/services/token.service.js
@@ -5,8 +5,12 @@ export const CreateTokens = (id) => {
   const accessToken = jsonwebtoken.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "20s",
   });
-  const refreshToken = jsonwebtoken.sign({ id }, process.env.REFRESH_SECRET, {
-    expiresIn: "1d",
-  });
+  const refreshToken = jsonwebtoken.sign(
+    { id },
+    process.env.JWT_REFRESH_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
   return { accessToken: accessToken, refreshToken: refreshToken };
 };
